ec264c677f5d1eb22075f2938c7ca2d487267407
Yevhen  (Jay) Zhdanov <yevhen.zhdanov@3ss.tv>
2021-12-14 23:17:56 +0200

    TypeScript Basics & Basic Types

---
+++ basics.ts
@@ -0,0 +1,20 @@
+function add(
+  n1: number,
+  n2: number,
+  canPrint: boolean = false,
+  printPhrase: string = ""
+) {
+  if (!canPrint) return n1 + n2;
+  if (printPhrase) console.log(printPhrase, n1 + n2);
+  console.log(n1 + n2);
+  return;
+}
+const num1 = 5;
+let num2: number; //if need => good practice
+num2 = 22.3;
+
+const num3: number = 22.3; // bad practice
+
+const printResult = true;
+add(num1, num2, printResult, "Result is: ");
+// add(+"5", 21.5, printResult);
---
+++ union-alisaea.ts
@@ -0,0 +1,54 @@
+type Combinable = number | string;
+type ConventionDescriptor = number | string;
+
+function combine(
+  input1: Combinable,
+  input2: Combinable,
+  resultConvention: ConventionDescriptor
+) {
+  let res;
+
+  if (
+    typeof input1 === "number" &&
+    typeof input2 === "number" &&
+    resultConvention === Convention.number
+  ) {
+    res = +input1 + +input2;
+  } else if (
+    typeof input1 === "number" &&
+    typeof input2 === "number" &&
+    resultConvention === Convention.text
+  ) {
+    res = (input1 + input2).toString();
+  } else {
+    res = input1.toString() + input2.toString();
+  }
+  console.log("res :>> ", res);
+  return res;
+}
+enum Convention {
+  number = "as-number",
+  text = "as-text",
+}
+
+// const combineNum = combine(32, 18);
+const combineNumToStr = combine(4, 2, Convention.text);
+
+const combineStr = combine("Anna", " is not a dog", Convention.text);
+
+type User = { name: string; age: number };
+
+const user1: { name: string; age: number } = {
+  name: "Max",
+  age: 32,
+};
+// => the same
+const user2: User = { name: "Pain", age: 11 };
+
+function greet(user: User) {
+  console.log(`hello ` + user.name);
+}
+
+greet({ name: "Jay" }); //=> not correct
+greet({ name: "Jay", age: 55 });
+greet(user2);
